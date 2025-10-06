bfdfe031706234c43590f56b81caa00907f5151f
sidoschn <dominik.schneidereit@fau.de>
2025-10-06 16:11:36 +0200

    updated generated dashboard

--- dashboardGenerator.py
+++ dashboardGenerator.py
@@ -25,14 +25,16 @@ def generateDashboard(definedkey, deviceid, jsondate):
     
     dashboardConfig = {"views":[{"title":"Grott Generated Dashboard","sections":[{"type":"grid", "cards":[{"type":"heading", "heading":"No Inverters detected yet"}]}]}]}
 
-    
+    sensorNameTag = "sensor."+deviceid.lower()
 
     dashboardSections = dashboardConfig["views"][0]["sections"]
 
     newSection = {"type":"grid", "cards":[]}
     sectionHeader = {"type":"heading", "heading":deviceid}
     newSection["cards"].append(sectionHeader)
-    pvInGauge = {"type":"gauge", "entity":"sensor.dlp0dyt037_pv_all_power", "name":"PV Eingangsleistung"}
+    pvInGauge = {"type":"gauge", "entity":sensorNameTag+"_pv_all_power", "name":"PV Eingangsleistung"}
+    newSection["cards"].append(pvInGauge)
+    bat01Gauge = {"type":"gauge", "entity":sensorNameTag+"_battery_1_soc", "name":"Ladestand Batterie 1"}
     newSection["cards"].append(pvInGauge)
     #dashboardSections.append(newSection)
     dashboardConfig["views"][0]["sections"][0]= newSection
