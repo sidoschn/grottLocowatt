ec2b42eb834eb038e78957e170283e12fcb9af72
sidoschn <dominik.schneidereit@fau.de>
2025-10-08 09:17:44 +0200

    added battery card visibility conditionals for both batteries using bdconoffstate enum

--- dashboardGenerator.py
+++ dashboardGenerator.py
@@ -35,22 +35,32 @@ def generateDashboard(definedkey, deviceid, jsondate):
         #battery charge sensor (with +-)
 
         #fill the new section with cards
+        # power related cards: 
         newSection["cards"].append({"type":"heading", "heading":deviceid}) #heading of the section
         newSection["cards"].append({"type":"heading", "heading": "Last Update:", "badges":[{"type":"entity","entity":sensorNameTag+"last_update"}]}) #time of last update (in header)
         newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"pvpowerin", "name":"PV Eingangsleistung", "grid_options":{"columns":6,"rows":"auto"}, "max":definedkey["opfullwatt"]}) #total input of PV panels
         newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"pvpowerout", "name":"Inverter Ausgangsleistung", "grid_options":{"columns":6,"rows":"auto"}, "max":definedkey["opfullwatt"]}) #total output of inverter
         newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"ptoloadtotal", "name":"Eigenverbrauch", "grid_options":{"columns":6,"rows":"auto"}, "max":definedkey["opfullwatt"]}) #total self conumed power
         newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"pgridimportexport", "name":"Netz Exportleistung", "grid_options":{"columns":6,"rows":"auto"}, "severity":{"green":0,"yellow":-definedkey["opfullwatt"],"red":(-definedkey["opfullwatt"]-10)}, "max":definedkey["opfullwatt"], "min":-definedkey["opfullwatt"], "needle":"true"}) #power exported to grid
-        # battery related
-        newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"pbdc1chrdischr", "name":"Batterie 1 Ladeleistung", "grid_options":{"columns":6,"rows":"auto"}, "severity":{"green":0,"yellow":-definedkey["opfullwatt"],"red":(-definedkey["opfullwatt"]-10)}, "max":(definedkey["opfullwatt"]/2), "min":-(definedkey["opfullwatt"]/2), "needle":"true"}) #power exported to grid
         
-        if not(definedkey["bdc2_pdischr"] > definedkey["opfullwatt"]):
+        # battery related cards:
+        # checking for battery states before displaying the battery cards
+        # bcdonoffstates: 0=no battery, 1 = battery1 connected, 2 = battery2 connected, 3 = battery1 and battery2 connected
+              
+        if (definedkey["bdconoffstate"] == 1 or definedkey["bdconoffstate"] == 3): 
+            newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"pbdc1chrdischr", "name":"Batterie 1 Ladeleistung", "grid_options":{"columns":6,"rows":"auto"}, "severity":{"green":0,"yellow":-definedkey["opfullwatt"],"red":(-definedkey["opfullwatt"]-10)}, "max":(definedkey["opfullwatt"]/2), "min":-(definedkey["opfullwatt"]/2), "needle":"true"}) #power exported to grid
+        
+        if (definedkey["bdconoffstate"] > 1): 
             newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"pbdc2chrdischr", "name":"Batterie 2 Ladeleistung", "grid_options":{"columns":6,"rows":"auto"}, "severity":{"green":0,"yellow":-definedkey["opfullwatt"],"red":(-definedkey["opfullwatt"]-10)}, "max":(definedkey["opfullwatt"]/2), "min":-(definedkey["opfullwatt"]/2), "needle":"true"}) #power exported to grid
         
-        newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"bdc1_soc", "name":"Ladestand Batterie 1", "severity":{"green":50,"yellow":15,"red":0}, "grid_options":{"columns":6,"rows":"auto"}})
+        if (definedkey["bdconoffstate"] == 1 or definedkey["bdconoffstate"] == 3): 
+            newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"bdc1_soc", "name":"Ladestand Batterie 1", "severity":{"green":50,"yellow":15,"red":0}, "grid_options":{"columns":6,"rows":"auto"}})
         
-        if not(definedkey["bdc2_pdischr"] > definedkey["opfullwatt"]):
+        if (definedkey["bdconoffstate"] > 1): 
             newSection["cards"].append({"type":"gauge", "entity":sensorNameTag+"bdc2_soc", "name":"Ladestand Batterie 2", "severity":{"green":50,"yellow":15,"red":0}, "grid_options":{"columns":6,"rows":"auto"}})
+
+
+        # history graph cards
 
         entitiesToAdd =[]
         for i in range(10):
