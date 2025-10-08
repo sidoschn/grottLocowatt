fa453ad0c4de78990807fb41d62cbbbb63e12112
sidoschn <dominik.schneidereit@fau.de>
2025-10-08 12:20:27 +0200

    reverted ini to point at growatt servers by default, changed fallback port to be int instead of str

--- grottproxy.py
+++ grottproxy.py
@@ -121,7 +121,7 @@ class Proxy:
         self.server.listen(200)
         self.forward_to = (conf.growattip, conf.growattport)
         # ! these fallback port ip and port options need to be moved to the config at some point
-        self.forward_to_fallback = ("127.0.0.1", "5280")
+        self.forward_to_fallback = ("127.0.0.1", 5280)
         
     def main(self,conf):
         self.input_list.append(self.server)
