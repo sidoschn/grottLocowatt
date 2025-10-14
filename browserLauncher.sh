e8d0972a882314d6595f532afbece2e34a083b5b
sidoschn <dominik.schneidereit@fau.de>
2025-10-14 15:15:50 +0200

    fixed bash command

--- browserLauncher.sh
+++ browserLauncher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chromium http://127.0.0.1/solar-board/0 --kiosk --window-size=720,1280 --window-position=0,0 --no-first-run --noerrdialogs
+startx /usr/bin/chromium http://127.0.0.1/solar-board/0 --kiosk --window-size=720,1280 --window-position=0,0 --no-first-run --noerrdialogs
