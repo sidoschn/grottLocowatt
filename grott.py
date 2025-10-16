cf9249e7b3d1141bed925341b1a89506411e8f8b
sidoschn <dominik.schneidereit@fau.de>
2025-10-16 15:46:39 +0200

    fixed syntax

--- grott.py
+++ grott.py
@@ -70,7 +70,8 @@ elif pullResult[0:7] == "Updatin":
 
     
     if bPsutil:
-         if print(psutil.Process(os.getpid()).ppid())== 1:
+         print("process ID is:" +str(psutil.Process(os.getpid()).ppid()))
+         if (psutil.Process(os.getpid()).ppid())== 1:
               #the script is run as a service, just exiting will restart it
               exit()
     
