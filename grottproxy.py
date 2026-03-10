2dbc7d758b428decbf1e56433b9d6ab16595924d
sidoschn <dominik.schneidereit@fau.de>
2026-03-10 20:05:21 +0100

    removed not required verbosity

--- grottproxy.py
+++ grottproxy.py
@@ -368,7 +368,7 @@ class Proxy:
         print(">>header:")
         header = "".join("{:02x}".format(n) for n in data[0:8])
         print(header)
-        print(bytes.fromhex(header))
+        #print(bytes.fromhex(header))
         # print(">> raw data:")
         # print(data)
         # print(">> reforamtted data:")
