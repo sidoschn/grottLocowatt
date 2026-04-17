bcfe17ae618ec139256686d3bcb83416010a9170
sidoschn <dominik.schneidereit@fau.de>
2026-04-17 15:24:28 +0200

    swtitched from looping daemon thread to GPIO event handler for NA pin event detection. added a "switch system" function to simplify switching on/off

--- grottNAcontrol.py
+++ grottNAcontrol.py
@@ -23,10 +23,13 @@ class grottNAgpio:
         
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        naObserverThread = threading.Thread(target=self.nAobserver, daemon=True)
-        naObserverThread.start()
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.pinFalling,bouncetime=100)
+        GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.pinRising,bouncetime=100)
+        # --switched from looped daemon thread to GPIO event handler
+        # naObserverThread = threading.Thread(target=self.nAobserver, daemon=True)
+        # naObserverThread.start()
 
-    def nAobserver (self):
+    def nAobserver (self): #legacy
         print("NA observer is started")
         while True:
             if hasattr(self.currentProxy, "loggerId"):
@@ -38,20 +41,35 @@ class grottNAgpio:
         self.currentProxy = proxy
         self.attachedToLogger = proxy.loggerId
         print("proxy set")
+        self.getGPIOstate()
+        self.interpretGPIOstate()
 
     def setConfig(self, config):
         self.currentConfig = config
         print("config set")
 
     def getGPIOstate(self):
-        
         self.currentGPIOstate = GPIO.input(self.pin)
         #print(self.currentGPIOstate)
         
+    def pinFalling(self):
+        if hasattr(self.currentProxy, "loggerId"):
+            print("falling pin voltage")
+            self.switchSystem(self, False)
         
+    def pinRising(self):
+        if hasattr(self.currentProxy, "loggerId"):
+            print("rising pin voltage")
+            self.switchSystem(self, True)
+        
+    def switchSystem(self, state):
+        bTurnOff = state
+        print("Setting system turn off state to "+ str(bTurnOff))
+        self.bTurnOff = bTurnOff
+        command = self.currentProxy.compileCommand(self.currentConfig ,"TurnOff", bTurnOff)
+        self.currentProxy.injectCommand(self.currentConfig, command)
 
     def interpretGPIOstate(self):
-       
         match self.currentGPIOstate:
             case 0:
                 #print("Turning on System")
@@ -65,12 +83,6 @@ class grottNAgpio:
                 bTurnOff = True
 
         if not (bTurnOff == self.bTurnOff):
-            print("Setting system turn off state to "+ str(bTurnOff))
-            self.bTurnOff = bTurnOff
-            command = self.currentProxy.compileCommand(self.currentConfig ,"TurnOff", bTurnOff)
-            print(command)
-            self.currentProxy.injectCommand(self.currentConfig, command) # command injection disabled for testing
+            self.switchSystem(bTurnOff)
         else:
-            # print("no action required")
             ...
-            #print("no change to system state required system on is: " + str(bTurnOn) )
